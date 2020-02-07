2b68b1e7fc559676fcceddf1ac77598fad6ef5e9
ami566 <47269677+ami566@users.noreply.github.com>
2020-02-07 14:21:03 +0200

    Create Program.cs

---
+++ ConsoleMVC/Program.cs
@@ -0,0 +1,14 @@
+using System;
+using ConsoleMVC.Controllers;
+using ConsoleMVC.Model;
+
+namespace ConsoleMVC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TipCalculatorController t = new TipCalculatorController();
+        }
+    }
+}
